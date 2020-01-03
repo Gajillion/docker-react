c998da6a9f2da5a9f445bc55d97a753223691e98
Mark Juric <mark.juric@dexcom.com>
2020-01-02 16:03:10 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+		  Or... you know. Whatever.
         </p>
         <a
           className="App-link"
